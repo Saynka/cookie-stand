99f820e16d36438f9542d6246c2dc82571d429c0
kale lesko <sankakale@gmail.com>
2020-08-10 21:35:51 -0700

    day1 shinanigins

--- js/app.js
+++ js/app.js
@@ -1,16 +1,153 @@
 "use strict"
 
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+// random num of customers
+// Seattle cookie sale
+
+console.log('Seattle')
+function numOfGuests(min, max){
+  return Math.ceil(Math.random() * (max - min) + min)
+  }
+  
+  console.log(numOfGuests(23, 65));
+
+function sales(guestsperhour, avg) {
+  return guestsperhour * avg;
 }
 
-console.log(getRandomInt(3));
-// expected output: 0, 1 or 2
+var storeHoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
 
-console.log(getRandomInt(1));
-// expected output: 0
+for (var i = 0; i <= 13; i = i + 1){
+  var guesttest = numOfGuests(23, 65);
+ console.log(storeHoursArr[i], sales(guesttest, 6.3));
+  storeHoursArr[i], sales(guesttest, 6.3)
+}
+// tokyo cookie sales
+console.log('Tokyo')
+function numOfGuests(min, max){
+  return Math.ceil(Math.random() * (max - min) + min)
+  }
+  
+  console.log(numOfGuests(3, 24));
 
-console.log(Math.random());
-// expected output: a number between 0 and 1
+function sales(guestsperhour, avg) {
+  return guestsperhour * avg;
+}
+
+var storeHoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
+for (var i = 0; i <= 13; i = i + 1){
+  var guesttest = numOfGuests(3, 24);
+ console.log(storeHoursArr[i], sales(guesttest, 61.2));
+  storeHoursArr[i], sales(guesttest, 61.2)
+}
+
+// Dubai cookie sales
+console.log('Dubai')
+function numOfGuests(min, max){
+  return Math.ceil(Math.random() * (max - min) + min)
+  }
+  
+  console.log(numOfGuests(11, 38));
+
+function sales(guestsperhour, avg) {
+  return guestsperhour * avg;
+}
+
+var storeHoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
+for (var i = 0; i <= 13; i = i + 1){
+  var guesttest = numOfGuests(11, 38);
+ console.log(storeHoursArr[i], sales(guesttest, 3.7));
+  storeHoursArr[i], sales(guesttest, 3.7)
+}
+
+// Paris Cookie sales
+console.log('Paris')
+function numOfGuests(min, max){
+  return Math.ceil(Math.random() * (max - min) + min)
+  }
+  
+  console.log(numOfGuests(20, 38));
+
+function sales(guestsperhour, avg) {
+  return guestsperhour * avg;
+}
+
+var storeHoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
+for (var i = 0; i <= 13; i = i + 1){
+  var guesttest = numOfGuests(20, 38);
+ console.log(storeHoursArr[i], sales(guesttest, 2.3));
+  storeHoursArr[i], sales(guesttest, 2.3)
+}
+
+// Lima Cookie sales
+console.log('Lima')
+function numOfGuests(min, max){
+  return Math.ceil(Math.random() * (max - min) + min)
+  }
+  
+  console.log(numOfGuests(2, 16));
+
+function sales(guestsperhour, avg) {
+  return guestsperhour * avg;
+}
+
+var storeHoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
+for (var i = 0; i <= 13; i = i + 1){
+  var guesttest = numOfGuests(2, 16);
+ console.log(storeHoursArr[i], sales(guesttest, 4.6));
+  storeHoursArr[i], sales(guesttest, 4.6)
+}
+
+
+ 
+
+
+var seattle = {
+  min: 23,
+  max: 65,
+  avg: 6.3,
+}
+
+var tokyo = {
+  min: 3,
+  max: 24,
+  avg: 61.2,
+}
+
+
+var dubai = {
+  min: 11,
+  max: 38,
+  avg: 3.7,
+}
+
+
+var paris = {
+  min: 20,
+  max: 38,
+  avg: 2.3,
+}
+
+
+var lima = {
+  min: 2,
+  max: 16,
+  avg: 4.6,
+}
+
+// console.log(sales(30, 6.3), numOfGuests(23,65))
+
+// // DO SOME WORK on a branch,
+// // CONSOLE: git add (FILES)
+// // CONSOLE git commit -m " (short message about what you did)"
+// // CONSOLE git push origin (BRANCH YOU AR ON)
+// // GITHUB make pull request and merge
+// // CONSOLE git checkout master
+// // CONSOLE git pull origin master
+// // CONSOLE git checkout -b (NEW BRANCH NAME) 
+// // GO BACK TO TOP OF STACK
 
